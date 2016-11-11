952a48085dfdcc3473759c2cde1ae4bf1f5b59f9
Bryant E. McDonnell <bemcdonnell@gmail.com>
2016-11-10 21:18:02 -0500

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
