3718f8effc0f00b18701ef490744fbd76ce047f9
Bryant E. McDonnell <bemcdonnell@gmail.com>
2016-11-12 19:43:09 -0500

    Added Test - Current Time Function

---
+++ test/TestModel1_CurrentTime.py
@@ -0,0 +1,44 @@
+'''
+TestModel1_Current Time
+
+Author: Bryant E. McDonnell (EmNet LLC)
+Date: 11/12/2016
+
+
+'''
+import time
+import os
+import sys
+
+#point to location of the pyswmm file
+sys.path.append(os.getcwd()+'\\..\\pyswmm\\')
+
+from swmm5 import pyswmm
+from toolkitapi import *
+
+swmmobject = pyswmm('./TestModel1_weirSetting.inp',\
+                    './TestModel1_weirSetting.rpt',\
+                    './TestModel1_weirSetting.out')
+swmmobject.swmm_open()
+
+swmmobject.swmm_start()
+
+i = 0
+while(True):
+    
+    time = swmmobject.swmm_stride(600)
+    i+=1
+    print(swmmobject.swmm_getCurrentSimualationTime())
+    print(type(swmmobject.swmm_getCurrentSimualationTime()))
+     
+
+    if (time <= 0.0):
+        break
+    if i %144==0: print i
+    
+swmmobject.swmm_end()
+swmmobject.swmm_report()
+swmmobject.swmm_close()
+
+ 
+print("swmm_step() Check Passed")
