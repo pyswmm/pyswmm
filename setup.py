2e80a76897190689bbc4148216e1ff93c770e7bd
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-02-20 23:55:34 -0500

    Update setup.py and add requirements.txt

--- setup.py
+++ setup.py
@@ -11,43 +11,35 @@
 import sys
 
 # Third party imports
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 PY2 = sys.version_info.major == 2
 VERSION = '0.2.1'
-AUTHOR_NAME = 'Bryant E. McDonnell (EmNet LLC)'
-AUTHOR_EMAIL = 'bemcdonnell@gmail.com'
-
 
 REQUIREMENTS = []
-
 
 if PY2:
     REQUIREMENTS.append('enum34')
 
-
-setup(name='pyswmm',
-      version=VERSION,
-      description='Python Wrapper for SWMM5 API',
-      url='https://github.com/OpenWaterAnalytics/pyswmm/wiki',
-      author=AUTHOR_NAME,
-      author_email=AUTHOR_EMAIL,
-      install_requires=REQUIREMENTS,
-      package_dir={'': 'pyswmm'},
-      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-      package_data={'':
-                    ['swmmLinkedLibs/Windows/swmm5.dll',
-                     'LICENSE.txt']},
-      include_package_data=True,
-      license="BSD2 License",
-      keywords = "swmm5, swmm, hydraulics, hydrology, modeling, collection system",
-      classifiers=[
-          "Topic :: Scientific/Engineering",
-          "Topic :: Documentation :: Sphinx",
-          "Operating System :: Microsoft :: Windows",
-          "License :: OSI Approved :: BSD License",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: C",
-          "Development Status :: 4 - Beta",
-          ]
-      )
+setup(
+    name='pyswmm',
+    version=VERSION,
+    description='Python Wrapper for SWMM5 API',
+    url='https://github.com/OpenWaterAnalytics/pyswmm/wiki',
+    author='Bryant E. McDonnell (EmNet LLC)',
+    author_email='bemcdonnell@gmail.com',
+    install_requires=REQUIREMENTS,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_data={'': ['swmmLinkedLibs/Windows/swmm5.dll', 'LICENSE.txt']},
+    include_package_data=True,
+    license="BSD2 License",
+    keywords="swmm5, swmm, hydraulics, hydrology, modeling, collection system",
+    classifiers=[
+        "Topic :: Scientific/Engineering",
+        "Topic :: Documentation :: Sphinx",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: C",
+        "Development Status :: 4 - Beta",
+    ])
