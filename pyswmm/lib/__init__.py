c044e6d742e2a3dbc1c8db416850734209293bbc
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-02-22 00:18:09 -0500

    Rename libraries folder and add constants for libraries

---
+++ pyswmm/lib/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014 Bryant E. McDonnell
+#
+# Licensed under the terms of the BSD2 License
+# See LICENSE.txt for details
+# -----------------------------------------------------------------------------
+"""SWMM5 compiled libraries."""
+
+# Standard library imports
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Library paths
+LIB_SWMM_WIN_32 = os.path.join(HERE, 'windows', 'swmm5.dll')
