43a1ccc0224d6d6d2dd0f1aca195fb54d9d05a01
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-03-12 00:05:52 -0500

    added dll selector

--- pyswmm/lib/__init__.py
+++ pyswmm/lib/__init__.py
@@ -10,7 +10,60 @@
 # Standard library imports
 import os
 
+# Machine Architechture
+MACHINE_BITS = 8 * tuple.__itemsize__
+
+# Local Path
 HERE = os.path.abspath(os.path.dirname(__file__))
 
+# Platform Detection
+def _platform():
+    """ Folder based on platform """
+    if os.name == 'nt':
+        return 'windows'
+
 # Library paths
-LIB_SWMM_WIN_32 = os.path.join(HERE, 'windows', 'swmm5.dll').replace('\\', '/')
+if os.name == 'nt':
+    LIB_SWMM = os.path.join(HERE, _platform(),
+                            'swmm5.dll').replace('\\', '/')
+
+class _DllPath(object):
+    """
+    DllPath Object.
+    """
+    def __init__(self):
+        self._dll_loc = LIB_SWMM
+    @property
+    def dll_loc(self):
+        """Get/set DLL Name """
+        return self._dll_loc
+    @dll_loc.setter
+    def dll_loc(self, value):
+        """Set DLL Name """
+        self._dll_loc = value
+    def __call__(self):
+        """ """
+        return self._dll_loc
+
+#Initialize dll path object
+DLL_SELECTION = _DllPath()
+
+def use(arg):
+    """
+    Set the SWMM5 DLL
+
+    Examples:
+    >>> import pyswmm
+    >>> pyswmm.lib.use("TestDLL")
+
+    from pyswmm import Simulation
+    """
+    dir_name = os.path.dirname(LIB_SWMM)
+    if not arg.endswith('.dll'):
+        arg = arg + ".dll"
+    if os.path.isfile(os.path.join(HERE, _platform(),
+                                   arg).replace('\\', '/')):
+        DLL_SELECTION.dll_loc = os.path.join(HERE, _platform(),
+                                             arg).replace('\\', '/')
+    else:
+        raise(Exception("Library Not Found"))
