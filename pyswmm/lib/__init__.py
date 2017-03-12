cc5ad75c2eb15cc517580ca7e46339a34b148d81
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-03-12 17:26:13 -0500

    Updates to layout

--- pyswmm/lib/__init__.py
+++ pyswmm/lib/__init__.py
@@ -13,24 +13,20 @@ import os
 # Machine Architechture
 MACHINE_BITS = 8 * tuple.__itemsize__
 
-
-
 # Local Path
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 # Platform Detection
 def _platform():
-    """ Folder based on platform """
+    """Folder based on platform."""
     if os.name == 'nt':
         return 'windows'
-
 
 # Library paths
 if os.name == 'nt':
     LIB_SWMM = os.path.join(HERE, _platform(),
                             'swmm5.dll').replace('\\', '/')
-
 
 class _DllPath(object):
     """
@@ -38,26 +34,27 @@ class _DllPath(object):
     """
     def __init__(self):
         self._dll_loc = LIB_SWMM
+
     @property
     def dll_loc(self):
-        """Get/set DLL Name """
-        return self._dll_loc
-    @dll_loc.setter
-    def dll_loc(self, value):
-        """Set DLL Name """
-        self._dll_loc = value
-    def __call__(self):
-        """ """
+        """Get/set DLL Name."""
         return self._dll_loc
 
+    @dll_loc.setter
+    def dll_loc(self, value):
+        """Set DLL Name."""
+        self._dll_loc = value
+
+    def __call__(self):
+        """Caller returns DLL Name."""
+        return self._dll_loc
 
 # Initialize dll path object
 DLL_SELECTION = _DllPath()
 
-
 def use(arg):
     """
-    Set the SWMM5 DLL
+    Set the SWMM5 DLL.
 
     Examples:
     >>> import pyswmm
