c2225f6a82e55d914ccec6a207bfc95aaa07b058
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-05-02 21:54:01 -0400

    added support for SWMM 5.1.12 and exposed swmm_version and error function

---
+++ pyswmm/tests/test_general_engine.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014 Bryant E. McDonnell
+#
+# Licensed under the terms of the BSD2 License
+# See LICENSE.txt for details
+# -----------------------------------------------------------------------------
+
+# Local imports
+import pyswmm
+from pyswmm import Simulation
+from pyswmm.tests.data import MODEL_WEIR_SETTING_PATH
+
+
+def test_engine_version():
+    pyswmm.lib.use("swmm5.dll")
+    sim = Simulation(MODEL_WEIR_SETTING_PATH)
+    print(sim.engine_version)
+    assert sim.engine_version == sim.engine_version
+
+def test_runoff_error():
+    sim = Simulation(MODEL_WEIR_SETTING_PATH)
+    sim.execute()
+    print(sim.runoff_error)
+    assert sim.runoff_error >= 0.0
+
+def test_flow_routing_error():
+    sim = Simulation(MODEL_WEIR_SETTING_PATH)
+    sim.execute()
+    print(sim.flow_routing_error)
+    assert sim.flow_routing_error >= 0.0
+
+def test_quality_error():
+    sim = Simulation(MODEL_WEIR_SETTING_PATH)
+    sim.execute()
+    print(sim.quality_error)
+    assert sim.quality_error >= 0.0
