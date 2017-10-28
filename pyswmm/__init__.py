caa69e0fb8f22f830317abab3f61e808c6aaa48d
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-10-27 23:34:56 -0400

    Added Set Output Stage Wrapper

--- pyswmm/__init__.py
+++ pyswmm/__init__.py
@@ -14,12 +14,7 @@ from pyswmm.simulation import Simulation
 from pyswmm.subcatchments import Subcatchment, Subcatchments
 from pyswmm.system import SystemStats
 
-<<<<<<< 17520bd7dfbf1b20865f2a7f114e119d3d8b4614
-
 VERSION_INFO = (0, 4, 4, 'dev0')
-=======
-VERSION_INFO = (0, 5, 0, 'dev0')
->>>>>>> ciocheck
 __version__ = '.'.join(map(str, VERSION_INFO))
 __swmm_version__ = '5.2.0.dev0'
 __author__ = 'Bryant E. McDonnell (EmNet LLC) - bemcdonnell@gmail.com'
