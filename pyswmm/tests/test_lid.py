885369aece0ea9ab888df3d12d0241c91888fc22
Jenn Wu <jwu@emnet.net>
2019-07-24 22:44:27 -0400

    update lid unit test with pytest approx

--- pyswmm/tests/test_lid.py
+++ pyswmm/tests/test_lid.py
@@ -3,7 +3,8 @@ from pyswmm import Simulation
 from pyswmm import LidControls, LidGroups, LidUnit
 from pyswmm.swmm5 import PySWMM
 from pyswmm.tests.data import MODEL_LIDS_PATH
-
+from pytest import approx 
+UT_PRECISION = 1 #%
 
 def test_list_lid_controls():
     with Simulation(MODEL_LIDS_PATH) as sim:
@@ -53,19 +54,19 @@ def test_lid_group_params():
         for i, step in enumerate(sim):
             if i == 115:
                 assert(sim.current_time.strftime('%Y-%m-%d %H:%M:%S') == '1998-01-02 05:00:00')
-                assert(sub_2_lid_units.pervious_area == 50000)
-                assert(round(sub_2_lid_units.flow_to_pervious, 4) == 0)
-                assert(round(sub_2_lid_units.old_drain_flow, 4) >= 0.0008)
-                assert(round(sub_2_lid_units.new_drain_flow, 4) >= 0.0008)
-
+                assert(sub_2_lid_units.pervious_area == approx(50000, rel=UT_PRECISION))
+                assert(sub_2_lid_units.flow_to_pervious == approx(0, rel=UT_PRECISION))
+                assert(sub_2_lid_units.old_drain_flow == approx(0.0008, rel=UT_PRECISION))
+                assert(sub_2_lid_units.new_drain_flow == approx(0.0008, rel=UT_PRECISION))
+                
 def test_lid_unit_params():
     with Simulation(MODEL_LIDS_PATH) as sim:
         sub_2_lid_units = LidGroups(sim)["2"]
         first_unit = sub_2_lid_units[0]
-        assert(first_unit.unit_area == 10000)
-        assert(first_unit.full_width == 20)
-        assert(first_unit.initial_saturation == 0)
-        assert(first_unit.from_impervious == 50)
+        assert(first_unit.unit_area == approx(10000, rel=UT_PRECISION))
+        assert(first_unit.full_width == approx(20, rel=UT_PRECISION))
+        assert(first_unit.initial_saturation == approx(0, rel=UT_PRECISION))
+        assert(first_unit.from_impervious == approx(50, rel=UT_PRECISION))
         assert(first_unit.index == 0)
         assert(first_unit.number == 4)
         assert(first_unit.drain_subcatchment == -1)
@@ -73,59 +74,59 @@ def test_lid_unit_params():
 
         for i, step in enumerate(sim):
             if i == 2145:
-                assert(first_unit.water_balance.inflow >= 8.9)
-                assert(first_unit.water_balance.evaporation == 0)
-                assert(first_unit.water_balance.infiltration >= 0.014)
-                assert(first_unit.water_balance.surface_flow == 0)
-                assert(first_unit.water_balance.drain_flow >= 3.35)
-                assert(first_unit.water_balance.initial_volume == 3)
-                assert(first_unit.water_balance.final_volume >= 8.5)
-                assert(first_unit.surface.depth == 0)
-                assert(first_unit.pavement.depth == 0)
-                assert(first_unit.soil.moisture >= 0.2)
-                assert(first_unit.storage.depth >= 6)
-                assert(first_unit.dry_time == 21600)
-                assert(first_unit.old_drain_flow >= 0.0005)
-                assert(first_unit.new_drain_flow >= 0.0005)
-                assert(first_unit.surface.flux_rate == 0)
+                assert(first_unit.water_balance.inflow == approx(8.9, rel=UT_PRECISION))
+                assert(first_unit.water_balance.evaporation == approx(0, rel=UT_PRECISION))
+                assert(first_unit.water_balance.infiltration == approx(0.014, rel=UT_PRECISION))
+                assert(first_unit.water_balance.surface_flow == approx(0, rel=UT_PRECISION))
+                assert(first_unit.water_balance.drain_flow == approx(3.35, rel=UT_PRECISION))
+                assert(first_unit.water_balance.initial_volume == approx(3, rel=UT_PRECISION))
+                assert(first_unit.water_balance.final_volume == approx(8.5, rel=UT_PRECISION))
+                assert(first_unit.surface.depth == approx(0, rel=UT_PRECISION))
+                assert(first_unit.pavement.depth == approx(0, rel=UT_PRECISION))
+                assert(first_unit.soil.moisture == approx(0.2, rel=UT_PRECISION))
+                assert(first_unit.storage.depth == approx(6, rel=UT_PRECISION))
+                assert(first_unit.dry_time == approx(21600, rel=UT_PRECISION))
+                assert(first_unit.old_drain_flow == approx(0.0005, rel=UT_PRECISION))
+                assert(first_unit.new_drain_flow == approx(0.0005, rel=UT_PRECISION))
+                assert(first_unit.surface.flux_rate == approx(0, rel=UT_PRECISION))
 
 def test_lid_control_params():
     with Simulation(MODEL_LIDS_PATH) as sim:
         LID = LidControls(sim)["LID"]
         Green_Lid = LidControls(sim)["Green_LID"]
 
-        assert(LID.surface.thickness == 10)
-        assert(LID.surface.void_fraction == 0.5)
-        assert(LID.surface.roughness == 0.013)
-        assert(LID.surface.slope == 1)
-        assert(LID.surface.side_slope == 5)
-        assert(round(LID.surface.alpha, 4) == 11.4615)
+        assert(LID.surface.thickness == approx(10, rel=UT_PRECISION))
+        assert(LID.surface.void_fraction == approx(0.5, rel=UT_PRECISION))
+        assert(LID.surface.roughness == approx(0.013, rel=UT_PRECISION))
+        assert(LID.surface.slope == approx(1, rel=UT_PRECISION))
+        assert(LID.surface.side_slope == approx(5, rel=UT_PRECISION))
+        assert(LID.surface.alpha == approx(11.4615, rel=UT_PRECISION))
         assert(LID.can_overflow == False)
-        assert(LID.soil.thickness == 30)
-        assert(LID.soil.porosity == 0.5)
-        assert(LID.soil.field_capacity == 0.2)
-        assert(LID.soil.wilting_point == 0.1)
-        assert(LID.soil.suction_head == 3.5)
-        assert(LID.soil.k_saturated == 5.0)
-        assert(LID.soil.k_slope == 10)
-        assert(LID.storage.thickness == 40)
-        assert(round(LID.storage.void_fraction, 4) == 0.75)
-        assert(LID.storage.k_saturated == 0.5)
-        assert(LID.storage.clog_factor == 0.2)
-        assert(LID.pavement.thickness == 20)
-        assert(round(LID.pavement.void_fraction, 4) == 0.15)
-        assert(LID.pavement.impervious_fraction == 0.5)
-        assert(LID.pavement.k_saturated == 100)
-        assert(LID.pavement.clog_factor == 8)
-        assert(LID.drain.coefficient == 0.5)
-        assert(LID.drain.exponent == 0.5)
-        assert(LID.drain.offset == 6)
-        assert(LID.drain.delay == 6)
+        assert(LID.soil.thickness == approx(30, rel=UT_PRECISION))
+        assert(LID.soil.porosity == approx(0.5, rel=UT_PRECISION))
+        assert(LID.soil.field_capacity == approx(0.2, rel=UT_PRECISION))
+        assert(LID.soil.wilting_point == approx(0.1, rel=UT_PRECISION))
+        assert(LID.soil.suction_head == approx(3.5, rel=UT_PRECISION))
+        assert(LID.soil.k_saturated == approx(5.0, rel=UT_PRECISION))
+        assert(LID.soil.k_slope == approx(10, rel=UT_PRECISION))
+        assert(LID.storage.thickness == approx(40, rel=UT_PRECISION))
+        assert(LID.storage.void_fraction == approx(0.75, rel=UT_PRECISION))
+        assert(LID.storage.k_saturated == approx(0.5, rel=UT_PRECISION))
+        assert(LID.storage.clog_factor == approx(0.2, rel=UT_PRECISION))
+        assert(LID.pavement.thickness == approx(20, rel=UT_PRECISION))
+        assert(LID.pavement.void_fraction == approx(0.15, rel=UT_PRECISION))
+        assert(LID.pavement.impervious_fraction == approx(0.5, rel=UT_PRECISION))
+        assert(LID.pavement.k_saturated == approx(100, rel=UT_PRECISION))
+        assert(LID.pavement.clog_factor == approx(8, rel=UT_PRECISION))
+        assert(LID.drain.coefficient == approx(0.5, rel=UT_PRECISION))
+        assert(LID.drain.exponent == approx(0.5, rel=UT_PRECISION))
+        assert(LID.drain.offset == approx(6, rel=UT_PRECISION))
+        assert(LID.drain.delay == approx(6, rel=UT_PRECISION))
         # no drainmat
-        assert(LID.drain_mat.thickness == 0)
-        assert(LID.drain_mat.void_fraction == 0)
-        assert(LID.drain_mat.roughness == 0)
-        assert(LID.drain_mat.alpha == 0)
+        assert(LID.drain_mat.thickness == approx(0, rel=UT_PRECISION))
+        assert(LID.drain_mat.void_fraction == approx(0, rel=UT_PRECISION))
+        assert(LID.drain_mat.roughness == approx(0, rel=UT_PRECISION))
+        assert(LID.drain_mat.alpha == approx(0, rel=UT_PRECISION))
 
 def test_lid_detailed_report():
     with Simulation(MODEL_LIDS_PATH) as sim:
@@ -146,11 +147,11 @@ def test_lid_detailed_report():
         for i, step in enumerate(sim):
             if i == 20:
                 assert(sim.current_time.strftime('%Y-%m-%d %H:%M:%S') == '1998-01-01 05:15:00')
-                assert(first_LID_unit_on_sub_2.surface.inflow >= 0.47)
-                assert(first_LID_unit_on_sub_2.evaporation == 0)
-                assert(first_LID_unit_on_sub_2.surface.infiltration >= 0.47)
-                assert(first_LID_unit_on_sub_2.pavement.percolation >= 0.47)
-                assert(first_LID_unit_on_sub_2.soil.percolation >= 0.615)
-                assert(first_LID_unit_on_sub_2.storage.exfiltration == 0)
-                assert(first_LID_unit_on_sub_2.surface.outflow == 0)
-                assert(first_LID_unit_on_sub_2.storage.drain == 0)
+                assert(first_LID_unit_on_sub_2.surface.inflow == approx(0.47, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.evaporation == approx(0, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.surface.infiltration == approx(0.47, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.pavement.percolation == approx(0.47, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.soil.percolation == approx(0.615, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.storage.exfiltration == approx(0, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.surface.outflow == approx(0, rel=UT_PRECISION))
+                assert(first_LID_unit_on_sub_2.storage.drain == approx(0, rel=UT_PRECISION))
