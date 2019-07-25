929fcc1970831e0ea5632e8395dd24dfc79b54b1
Jenn Wu <jwu@emnet.net>
2019-07-25 16:36:02 -0400

    remove omp file

--- pyswmm/lib/__init__.py
+++ pyswmm/lib/__init__.py
@@ -38,8 +38,6 @@ if os.name == 'nt':
         LIB_SWMM = os.path.join(HERE, _platform(), 'swmm5-x64.dll').replace('\\', '/')
     else:
         LIB_SWMM = os.path.join(HERE, _platform(), 'swmm5.dll').replace('\\', '/')
-#    LIB_OMP = os.path.join(HERE, _platform())
-#    os.environ['PATH'] = LIB_OMP + os.pathsep + os.environ['PATH']
 elif sys.platform == 'darwin':
     LIB_SWMM = os.path.join(HERE, _platform(), 'swmm5.dylib').replace('\\', '/')
 elif sys.platform.startswith('linux'):
