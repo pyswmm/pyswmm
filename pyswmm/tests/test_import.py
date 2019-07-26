df9c363776eb036bda1aa3c7b1d038054d329d98
Jenn Wu <jwu@emnet.net>
2019-07-25 20:58:42 -0400

    fix import unit test

--- pyswmm/tests/test_import.py
+++ pyswmm/tests/test_import.py
@@ -13,41 +13,47 @@ import sys
 # Local imports
 from pyswmm import Simulation
 from pyswmm.tests.data import MODEL_WEIR_SETTING_PATH, WIN_SWMM_LIB_PATH
+from pyswmm.lib import DLL_SELECTION
 import pyswmm
 
 
-##def test_use_1():
-##    if sys.platform == 'darwin':
-##        pyswmm.lib.use("swmm5.dylib")
-##    elif sys.platform.startswith('linux'):
-##        pyswmm.lib.use("swmm5.so")
-##    else:
-##        pyswmm.lib.use("swmm5.dll")
-##
-##
-##def test_use_2():
-##    pyswmm.lib.use("swmm5")
-##
-##
-##def test_use_3():
-##    try:
-##        pyswmm.lib.use("fakedll.dll")
-##    except:
-##        pass
-##
-##
-##def test_use_4():
-##    try:
-##        pyswmm.lib.use("fakedll")
-##    except:
-##        pass
-##
-##
-##def test_lib_5():
-##    """Testing SWMM Path as argument to Simulation Object. 
-##
-##    On Windows for now."""
-##    if os.name == 'nt':
-##        print(WIN_SWMM_LIB_PATH)
-##        sim = Simulation(MODEL_WEIR_SETTING_PATH, swmm_lib_path=WIN_SWMM_LIB_PATH) 
-##        sim.execute()
+def test_use_1():
+    if sys.platform == 'darwin':
+        pyswmm.lib.use("swmm5.dylib")
+    elif sys.platform.startswith('linux'):
+        pyswmm.lib.use("swmm5.so")
+    else:
+        pyswmm.lib.use("swmm5.dll")
+    pyswmm.lib.use(DLL_SELECTION())
+
+
+def test_use_2():
+    pyswmm.lib.use("swmm5")
+    pyswmm.lib.use(DLL_SELECTION())
+
+
+def test_use_3():
+    try:
+        pyswmm.lib.use("fakedll.dll")
+    except:
+        pass
+    pyswmm.lib.use(DLL_SELECTION())
+
+
+def test_use_4():
+    try:
+        pyswmm.lib.use("fakedll")
+    except:
+        pass
+    pyswmm.lib.use(DLL_SELECTION())
+
+
+def test_lib_5():
+    """Testing SWMM Path as argument to Simulation Object. 
+
+    On Windows for now."""
+    if os.name == 'nt':
+        print(WIN_SWMM_LIB_PATH)
+        sim = Simulation(MODEL_WEIR_SETTING_PATH, swmm_lib_path=WIN_SWMM_LIB_PATH) 
+        sim.execute()
+    pyswmm.lib.use(DLL_SELECTION())
