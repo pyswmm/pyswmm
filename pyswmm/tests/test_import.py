ae0c006388fb349100a49aa6e6613bbd85e5cb48
Jenn Wu <jwu@emnet.net>
2019-07-25 21:19:53 -0400

    update import test to check for exception

--- pyswmm/tests/test_import.py
+++ pyswmm/tests/test_import.py
@@ -14,37 +14,35 @@ import sys
 from pyswmm import Simulation
 from pyswmm.tests.data import MODEL_WEIR_SETTING_PATH, WIN_SWMM_LIB_PATH
 import pyswmm
-
+import pytest
 
 def test_use_1():
     if sys.platform == 'darwin':
-        pyswmm.lib.use("swmm5.dylib")
+        pyswmm.lib.use('swmm5.dylib')
     elif sys.platform.startswith('linux'):
-        pyswmm.lib.use("swmm5.so")
+        pyswmm.lib.use('swmm5.so')
         pyswmm.lib.use(DLL_SELECTION())
     else:
-        pyswmm.lib.use("swmm5.dll")
+        pyswmm.lib.use('swmm5.dll')
         pyswmm.lib.use(WIN_SWMM_LIB_PATH)
 
 
 def test_use_2():
-    pyswmm.lib.use("swmm5")
+    pyswmm.lib.use('swmm5')
     pyswmm.lib.use(WIN_SWMM_LIB_PATH)
 
 
 def test_use_3():
-    try:
-        pyswmm.lib.use("fakedll.dll")
-    except:
-        pass
+    with pytest.raises(Exception) as e:
+        assert pyswmm.lib.use('fakedll.dll')
+    assert str(e.value) == 'Library Not Found'
 
 
 def test_use_4():
-    try:
-        pyswmm.lib.use("fakedll")
-    except:
-        pass
-
+    with pytest.raises(Exception) as e:
+        pyswmm.lib.use('fakedll')
+    assert str(e.value) == 'Library Not Found'
+    
 
 def test_lib_5():
     """Testing SWMM Path as argument to Simulation Object. 
