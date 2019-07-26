2fcfa349837eff42fd483c7a24fc3e4e4565c170
Jenn Wu <jwu@emnet.net>
2019-07-25 21:05:43 -0400

    fix import unit test #3

--- pyswmm/tests/test_import.py
+++ pyswmm/tests/test_import.py
@@ -13,7 +13,6 @@ import sys
 # Local imports
 from pyswmm import Simulation
 from pyswmm.tests.data import MODEL_WEIR_SETTING_PATH, WIN_SWMM_LIB_PATH
-from pyswmm.lib import DLL_SELECTION
 import pyswmm
 
 
@@ -25,12 +24,12 @@ def test_use_1():
         pyswmm.lib.use(DLL_SELECTION())
     else:
         pyswmm.lib.use("swmm5.dll")
-        pyswmm.lib.use(DLL_SELECTION())
+        pyswmm.lib.use(WIN_SWMM_LIB_PATH)
 
 
 def test_use_2():
     pyswmm.lib.use("swmm5")
-    pyswmm.lib.use(DLL_SELECTION())
+    pyswmm.lib.use(WIN_SWMM_LIB_PATH)
 
 
 def test_use_3():
@@ -55,4 +54,4 @@ def test_lib_5():
         print(WIN_SWMM_LIB_PATH)
         sim = Simulation(MODEL_WEIR_SETTING_PATH, swmm_lib_path=WIN_SWMM_LIB_PATH) 
         sim.execute()
-        pyswmm.lib.use(DLL_SELECTION())
+        pyswmm.lib.use(WIN_SWMM_LIB_PATH)
