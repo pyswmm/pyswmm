8cc2a4baad9f18bd24ce2337c4360145a2270e3c
Jenn Wu <jwu@emnet.net>
2020-09-23 21:12:02 -0400

    remove warning check test for now

--- pyswmm/tests/test_warning.py
+++ pyswmm/tests/test_warning.py
@@ -4,11 +4,6 @@ import warnings
 from pyswmm.error import SWMM5FutureWarning
 
 
-def test_import_from_swmm5():
-    with pytest.warns(SWMM5FutureWarning):
-        from pyswmm.swmm5 import SWMMException
-
-
 def test_import_from_simulation():
     with pytest.warns(None) as record:
         from pyswmm.tests.data import MODEL_WEIR_SETTING_PATH
