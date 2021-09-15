2539c6e9bc31a6bcdcaf86d48b425a37b0b2a177
Jennifer Wu <wuu.jennifer@gmail.com>
2021-09-15 17:32:47 -0400

    Remove api attribute check since it is completed by swmm-python already

--- pyswmm/toolkitapi.py
+++ pyswmm/toolkitapi.py
@@ -533,50 +533,50 @@ class SMO_systemAttribute(Enum):
     evap_rate = 13
 
 
-subcatch_attribute = dict(
-    rainfall=shared_enum.SubcatchAttribute.RAINFALL,
-    snow_depth=shared_enum.SubcatchAttribute.SNOW_DEPTH,
-    evap_loss=shared_enum.SubcatchAttribute.EVAP_LOSS,
-    infil_loss=shared_enum.SubcatchAttribute.INFIL_LOSS,
-    runoff_rate=shared_enum.SubcatchAttribute.RUNOFF_RATE,
-    gw_outflow_rate=shared_enum.SubcatchAttribute.GW_OUTFLOW_RATE,
-    gw_table_elev=shared_enum.SubcatchAttribute.GW_TABLE_ELEV,
-    soil_moisture=shared_enum.SubcatchAttribute.SOIL_MOISTURE,
-    pollutant_concentration=shared_enum.SubcatchAttribute.POLLUT_CONC_0,
-)
-
-node_attribute = dict(
-    invert_depth=shared_enum.NodeAttribute.INVERT_DEPTH,
-    hydraulic_head=shared_enum.NodeAttribute.HYDRAULIC_HEAD,
-    ponded_volume=shared_enum.NodeAttribute.PONDED_VOLUME,
-    lateral_inflow=shared_enum.NodeAttribute.LATERAL_INFLOW,
-    total_inflow=shared_enum.NodeAttribute.TOTAL_INFLOW,
-    flooding_losses=shared_enum.NodeAttribute.FLOODING_LOSSES,
-    pollutant_concentration=shared_enum.NodeAttribute.POLLUT_CONC_0,
-)
-
-link_attribute = dict(
-    flow_rate=shared_enum.LinkAttribute.FLOW_RATE,
-    flow_depth=shared_enum.LinkAttribute.FLOW_DEPTH,
-    flow_velocity=shared_enum.LinkAttribute.FLOW_VELOCITY,
-    flow_volume=shared_enum.LinkAttribute.FLOW_VOLUME,
-    capacity=shared_enum.LinkAttribute.CAPACITY,
-    pollutant_concentration=shared_enum.LinkAttribute.POLLUT_CONC_0,
-)
-
-system_attribute = dict(
-    air_temp=shared_enum.SystemAttribute.AIR_TEMP,
-    rainfall=shared_enum.SystemAttribute.RAINFALL,
-    snow_depth=shared_enum.SystemAttribute.SNOW_DEPTH,
-    evap_infil_loss=shared_enum.SystemAttribute.EVAP_INFIL_LOSS,
-    runoff_flow=shared_enum.SystemAttribute.RUNOFF_FLOW,
-    dry_weather_inflow=shared_enum.SystemAttribute.DRY_WEATHER_INFLOW,
-    gw_inflow=shared_enum.SystemAttribute.GW_INFLOW,
-    rdii_inflow=shared_enum.SystemAttribute.RDII_INFLOW,
-    direct_inflow=shared_enum.SystemAttribute.DIRECT_INFLOW,
-    total_lateral_inflow=shared_enum.SystemAttribute.TOTAL_LATERAL_INFLOW,
-    flood_losses=shared_enum.SystemAttribute.FLOOD_LOSSES,
-    outfall_flows=shared_enum.SystemAttribute.OUTFALL_FLOWS,
-    volume_stored=shared_enum.SystemAttribute.VOLUME_STORED,
-    evap_rate=shared_enum.SystemAttribute.EVAP_RATE,
-)
+# subcatch_attribute = dict(
+#     rainfall=shared_enum.SubcatchAttribute.RAINFALL,
+#     snow_depth=shared_enum.SubcatchAttribute.SNOW_DEPTH,
+#     evap_loss=shared_enum.SubcatchAttribute.EVAP_LOSS,
+#     infil_loss=shared_enum.SubcatchAttribute.INFIL_LOSS,
+#     runoff_rate=shared_enum.SubcatchAttribute.RUNOFF_RATE,
+#     gw_outflow_rate=shared_enum.SubcatchAttribute.GW_OUTFLOW_RATE,
+#     gw_table_elev=shared_enum.SubcatchAttribute.GW_TABLE_ELEV,
+#     soil_moisture=shared_enum.SubcatchAttribute.SOIL_MOISTURE,
+#     pollutant_concentration=shared_enum.SubcatchAttribute.POLLUT_CONC_0,
+# )
+#
+# node_attribute = dict(
+#     invert_depth=shared_enum.NodeAttribute.INVERT_DEPTH,
+#     hydraulic_head=shared_enum.NodeAttribute.HYDRAULIC_HEAD,
+#     ponded_volume=shared_enum.NodeAttribute.PONDED_VOLUME,
+#     lateral_inflow=shared_enum.NodeAttribute.LATERAL_INFLOW,
+#     total_inflow=shared_enum.NodeAttribute.TOTAL_INFLOW,
+#     flooding_losses=shared_enum.NodeAttribute.FLOODING_LOSSES,
+#     pollutant_concentration=shared_enum.NodeAttribute.POLLUT_CONC_0,
+# )
+#
+# link_attribute = dict(
+#     flow_rate=shared_enum.LinkAttribute.FLOW_RATE,
+#     flow_depth=shared_enum.LinkAttribute.FLOW_DEPTH,
+#     flow_velocity=shared_enum.LinkAttribute.FLOW_VELOCITY,
+#     flow_volume=shared_enum.LinkAttribute.FLOW_VOLUME,
+#     capacity=shared_enum.LinkAttribute.CAPACITY,
+#     pollutant_concentration=shared_enum.LinkAttribute.POLLUT_CONC_0,
+# )
+#
+# system_attribute = dict(
+#     air_temp=shared_enum.SystemAttribute.AIR_TEMP,
+#     rainfall=shared_enum.SystemAttribute.RAINFALL,
+#     snow_depth=shared_enum.SystemAttribute.SNOW_DEPTH,
+#     evap_infil_loss=shared_enum.SystemAttribute.EVAP_INFIL_LOSS,
+#     runoff_flow=shared_enum.SystemAttribute.RUNOFF_FLOW,
+#     dry_weather_inflow=shared_enum.SystemAttribute.DRY_WEATHER_INFLOW,
+#     gw_inflow=shared_enum.SystemAttribute.GW_INFLOW,
+#     rdii_inflow=shared_enum.SystemAttribute.RDII_INFLOW,
+#     direct_inflow=shared_enum.SystemAttribute.DIRECT_INFLOW,
+#     total_lateral_inflow=shared_enum.SystemAttribute.TOTAL_LATERAL_INFLOW,
+#     flood_losses=shared_enum.SystemAttribute.FLOOD_LOSSES,
+#     outfall_flows=shared_enum.SystemAttribute.OUTFALL_FLOWS,
+#     volume_stored=shared_enum.SystemAttribute.VOLUME_STORED,
+#     evap_rate=shared_enum.SystemAttribute.EVAP_RATE,
+# )
