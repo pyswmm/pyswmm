a019e0733229f7cfab07131cf3776b53ed2e9c34
karosc <ckaros@outlook.com>
2023-01-24 21:10:09 -0500

    set release version

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python",
         "Development Status :: 5 - Production/Stable",
     ])
