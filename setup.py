6b09452c37091a84fa0cd5d9c15e8433f8696f20
karosc <ckaros@outlook.com>
2023-03-28 14:02:15 -0400

    add ability to install pyswmm with specific versions of OWA swmm engine

--- setup.py
+++ setup.py
@@ -51,6 +51,14 @@ setup(
     url='https://github.com/OpenWaterAnalytics/pyswmm/wiki',
     author='Bryant E. McDonnell (See AUTHORS)',
     install_requires=REQUIREMENTS,
+    extras_require = {
+        '5.1.14':['swmm-toolkit==0.9.1'],
+        '5.1.15':['swmm-toolkit==0.10.0'],
+        '5.2.0' :['swmm-toolkit==0.11.0'],
+        '5.2.1' :['swmm-toolkit==0.12.0'],
+        '5.2.2' :['swmm-toolkit==0.13.0'],
+        '5.2.3' :['swmm-toolkit==0.14.0'],
+    },
     packages=find_packages(exclude=['contrib', 'docs']),
     package_data={
         '': ['LICENSE.txt', 'AUTHORS', 'tests/data/*.inp', 'tests/*.py']
