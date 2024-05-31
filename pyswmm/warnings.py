2732fad74d84c2b7f5d6235d4cb1a08091350658
timothy-holmes <46288522+timothy-holmes@users.noreply.github.com>
2024-05-31 14:05:46 +1000

    reword default SimulationContextWarning message
    Co-authored-by: Constantine Karos <36245370+karosc@users.noreply.github.com>

--- pyswmm/warnings.py
+++ pyswmm/warnings.py
@@ -5,7 +5,7 @@ class SimulationContextWarning(ResourceWarning):
         message -- explanation of the error
     """
     message = """
-    \tThe Simulation object is intended for use with the context
-    \tmanager. System resources will not be freed
-    \twithout calling close method. See Simulation docs for details.
+    \tThe Simulation object is intended to be used with a context
+    \tmanager. System resources will not be freed without calling 
+    \tthe close method. See Simulation class docs for details.
     """
